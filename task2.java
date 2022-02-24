6dd45d2cfc0e63e79f7fbe39059233a9a3b118f6
Hans Johannessen <tonyhbj@gmail.com>
2022-02-24 13:22:03 +0100

    Add Try
    Add Try so the java.util.Scanner is gonna only try once, and then stop, so the method don't keep running

--- task2.java
+++ task2.java
@@ -4,21 +4,21 @@ public class task2 {
 
     public static void main(String[] args) {
     
-       Scanner scanner = new Scanner(System.in);
-       
-       System.out.println("What is your name? ");
-       String name = scanner.nextLine();
-       System.out.println();
+       try (Scanner scanner = new Scanner(System.in)) {
+        System.out.println("What is your name? ");
+           String name = scanner.nextLine();
+           System.out.println();
 
-       System.out.println("What is your age? ");
-       String age = scanner.nextLine();
-       System.out.println();
+           System.out.println("What is your age? ");
+           String age = scanner.nextLine();
+           System.out.println();
 
-       System.out.println("Do you clap your hands? ");
-       String dont = scanner.nextLine();
-       // Ved ikke hvordan jeg laver en boolean her, da det allerede er i en class for sig selv i main
-       System.out.println();
+           System.out.println("Do you clap your hands? ");
+           String dont = scanner.nextLine();
+           // Ved ikke hvordan jeg laver en boolean her, da det allerede er i en class for sig selv i main
+           System.out.println();
 
-       System.out.print("My name is " + name + ",and I am " + age + " years old, and i " + dont + " clap my hands");
+           System.out.print("My name is " + name + ",and I am " + age + " years old, and i " + dont + " clap my hands");
+    }
     }
 }
